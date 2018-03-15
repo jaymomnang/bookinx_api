fda44ecfbe1213bdd162bb5f6d9ee451e91ef7f2
jaymomnang <juliusmomnang@gmail.com>
2018-03-15 03:14:19 +0100

    bug fixes saving schedules

--- controllers/schedulesController.js
+++ controllers/schedulesController.js
@@ -3,99 +3,90 @@ var mongoose = require('mongoose'),
     //schema = mongoose.Schema,
     schedules = mongoose.model('schedules');
 
-exports.list_all_schedules = function(req, res) {
-    schedules.find({}, function(err, schedules) {
+exports.list_all_schedules = function (req, res) {
+    schedules.find({}, function (err, schedules) {
         if (err)
             res.send(err);
         res.json(schedules);
     });
 };
 
-exports.create_schedule = function(req, res) {
+exports.create_schedule = function (req, res) {
     var new_schedule = new schedules(req.body);
-    
 
-    schedules.findOne({}, 'schedule_id').sort({schedule_id: -1}).exec(function(error, data){
+
+    schedules.findOne({}, 'schedule_id').sort({
+        schedule_id: -1
+    }).exec(function (error, data) {
         if (error) res.send(error)
-    
+
         var last_scheduleId = 'SCH0000000';
-        if (data != null){
+        if (data != null) {
             last_scheduleId = data.schedule_id;
         }
 
         new_schedule.schedule_id = getNewtaskId(last_scheduleId);
-        
-        new_schedule.save(function(err, new_schedule) {
-            if (err) {
-                res.send(err);
 
-                schedules.find({}, function(err, schedules) {
-                    if (err)
-                        res.send(err);
-                    res.json(schedules);
-                });
-            }
+        new_schedule.save(function (err, new_schedule) {
+            if (err) res.send(err);
+
+            schedules.find({}, function (err, schedules) {
+                if (err)
+                    res.send(err);
+                res.json(schedules);
+            });
         });
     });
-    
+
 };
 
-exports.get_schedule = function(req, res) {
-    schedules.findById(req.params.schedule_id, function(err, schedule) {
+exports.get_schedule = function (req, res) {
+    schedules.findById(req.params.schedule_id, function (err, schedule) {
         if (err)
             res.send(err);
         res.json(schedule);
     });
 };
 
-exports.update_schedule = function(req, res) {
-    schedules.findOneAndUpdate({ schedule_id: req.params.scheduleId }, req.body, { new: true }, function(err, schedule) {
+exports.update_schedule = function (req, res) {
+    schedules.findOneAndUpdate({
+        schedule_id: req.params.scheduleId
+    }, req.body, {
+        new: true
+    }, function (err, schedule) {
         if (err)
             res.send(err);
         res.json(schedule);
     });
 };
 
-exports.delete_schedule = function(req, res) {
+exports.delete_schedule = function (req, res) {
     schedules.remove({
         schedule_id: req.params.schedule_id
-    }, function(err, prd) {
+    }, function (err, prd) {
         if (err)
             res.send(err);
         var message = 'schedule successfully deleted';
-        schedules.find({}, function(err, schedules) {
+        schedules.find({}, function (err, schedules) {
             if (err)
                 res.send(err);
-            res.json({ schedules, message });
+            res.json({
+                schedules,
+                message
+            });
         });
     });
 };
 
 //internally generated numbers for attendance records.
-var getNewtaskId = function(currentID){
-    var pos = Number(currentID.substring(3,10)) + 1;
-    var nxt = "SCH000000";
-    switch(pos.toString().length) {
-        case 2:
-            nxt = "SCH00000" + pos.toString();
-            break;
-        case 3:
-            nxt = "SCH0000" + pos.toString();
-            break;
-        case 4:
-            nxt = "SCH000" + pos.toString();
-            break;
-        case 5:
-            nxt = "SCH00" + pos.toString();
-            break;
-        case 6:
-            nxt = "SCH0" + pos.toString();
-            break;
-        case 7:
-            nxt = "SCH" + pos.toString();
-            break;
-        default:
-            nxt = nxt + pos.toString();
-  }
-  return nxt;
+var getNewtaskId = function (currentID) {
+    var pos = Number(currentID.substring(3, 10)) + 1;
+    var l = pos.toString().length;
+    var nxt = "SCH";
+
+    for (var i = 0; i + l < 7; i++) {
+        nxt = nxt + "0";
+    }
+
+    return nxt + pos.toString();
 };
